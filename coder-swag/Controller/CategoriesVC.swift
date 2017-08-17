80199068075cec95c0e9d183f383ce05ad9d6968
Jake Bush <jackburton@Jack-Burton.local>
2017-08-17 08:04:00 -0700

    added table views

---
+++ coder-swag/Controller/CategoriesVC.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Jake Bush on 8/17/17.
+//  Copyright © 2017 PorkChopExpress. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+    
+    @IBOutlet weak var categoryTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
