57b815117c51829781fb785d4f0a18c77945e18d
Jake Bush <jackburton@Jack-Burton.local>
2017-08-17 13:32:36 -0700

    created custom table view cell and implemented delegate functions

---
+++ coder-swag/Services/DataService.swift
@@ -0,0 +1,24 @@
+//
+//  DataService.swift
+//  coder-swag
+//
+//  Created by Jake Bush on 8/17/17.
+//  Copyright © 2017 PorkChopExpress. All rights reserved.
+//
+
+import Foundation
+
+class DataService {
+    static let instance = DataService()
+    
+    private let categories = [
+        Category(title: "SHIRTS", imageName: "shirts.png"),
+        Category(title: "HOODIES", imageName: "hoodies.png"),
+        Category(title: "HATS", imageName: "hats.png"),
+        Category(title: "DIGITAL", imageName: "digital.png")
+    ]
+    
+    func getCategories() -> [Category] {
+        return categories
+    }
+}
