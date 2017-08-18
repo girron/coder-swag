9159f67991540ac806e497f927b80fa9bba4615b
Jake Bush <jackburton@Jack-Burton.local>
2017-08-18 12:19:56 -0700

    designed custom UI Collection Cell

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Jake Bush on 8/18/17.
+//  Copyright © 2017 PorkChopExpress. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
